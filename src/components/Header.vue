ce06e5c31bb4b2594983995ef6917141a4de132c
Christopher Dang <58712502+christopherdang@users.noreply.github.com>
2022-03-29 14:21:47 -0400

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <div>
+            <h1>COVID-19 Database Application</h1>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    components: {
+
+    }
+}
+</script>
